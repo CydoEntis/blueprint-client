c18596514753234fee0f2adf9cd51d29e93f90c4
CydoEntis <cydoentis@gmail.com>
2022-12-31 18:59:56 -0500

    Added side bar to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,16 +5,30 @@ type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
-    <section className="flex flex-wrap items-center gap-5 py-5">
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
+    <section className="flex py-5">
+      <main className="flex h-4/5 flex-wrap items-center gap-3 p-3 md:w-10/12">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </main>
+      <article className="mx-auto flex h-4/5 flex-wrap items-center justify-center gap-3 p-3 md:w-2/12">
+        <div>Something goes here</div>
+        <div>
+          <h1>Recent Activities</h1>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </article>
     </section>
   );
 };
