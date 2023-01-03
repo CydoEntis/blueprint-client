8cf76557cc19ee4ee6216622f530bd66716eb2b5
CydoEntis <cydoentis@gmail.com>
2023-01-03 06:46:04 -0500

    Stats, and Recent Activities components added

---
+++ src/components/recent/RecentActivitesFeed.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const RecentActivitesFeed = (props: Props) => {
+  return <div>RecentActivitesFeed</div>;
+};
+
+export default RecentActivitesFeed;
---
+++ src/components/stats/StatsBlock.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const StatsBlock = (props: Props) => {
+  return <div>StatsBlock</div>;
+};
+
+export default StatsBlock;
