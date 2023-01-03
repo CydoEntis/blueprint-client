09ad9c5342cb2862d7b6c26e69cff5e13b9b88d5
CydoEntis <cydoentis@gmail.com>
2023-01-03 09:15:49 -0500

    Finished projects navigation

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  iconColor: string;
+  title: string;
+};
 
-const Project = (props: Props) => {
-  return <div>Project</div>;
+const Project = ({ iconColor, title }: Props) => {
+  return (
+    <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
+      <div className={`mr-1 h-2 w-2 rounded-full bg-${iconColor}-450`}></div>
+      {title}
+    </li>
+  );
 };
 
 export default Project;
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,6 +3,7 @@ import Sidebar from "../sidebar/Sidebar";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import Category from "@/components/projects/Category";
+import Project from "./Project";
 
 type Props = {};
 
@@ -18,59 +19,24 @@ const Projects = (props: Props) => {
 
         {/* Favorites */}
         <Category title="favorites">
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-red-450"></div>
-            Project 1
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-yellow-450"></div>
-            Project 2
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-green-450"></div>
-            Project 3
-          </li>
+          <Project iconColor="red" title="Project 1" />
+          <Project iconColor="yellow" title="Project 2" />
+          <Project iconColor="green" title="Project 3" />
         </Category>
 
         {/* All Projects */}
-        <ul className="mt-8 flex w-full flex-col">
-          <li className="flex items-center py-1 text-xs uppercase text-grey-550">
-            <ChevronDownIcon className="mr-1 h-4 w-4" />
-            All Projects
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-red-450"></div>
-            Project 1
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-yellow-450"></div>
-            Project 2
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-green-450"></div>
-            Project 3
-          </li>
-        </ul>
+        <Category title="all projects">
+          <Project iconColor="red" title="Project 1" />
+          <Project iconColor="yellow" title="Project 2" />
+          <Project iconColor="green" title="Project 3" />
+        </Category>
 
         {/* Archived */}
-        <ul className="mt-8 flex w-full flex-col">
-          <li className="flex items-center py-1 text-xs uppercase text-grey-550">
-            <ChevronDownIcon className="mr-1 h-4 w-4" />
-            Archived
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-red-450"></div>
-            Project 1
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-yellow-450"></div>
-            Project 2
-          </li>
-          <li className="ml-2 flex items-center py-1 text-xs text-grey-550">
-            <div className="mr-1 h-2 w-2 rounded-full bg-green-450"></div>
-            Project 3
-          </li>
-        </ul>
+        <Category title="archived">
+          <Project iconColor="red" title="Project 1" />
+          <Project iconColor="yellow" title="Project 2" />
+          <Project iconColor="green" title="Project 3" />
+        </Category>
       </nav>
     </Sidebar>
   );
