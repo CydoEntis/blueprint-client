07613c48b60c445ed8b6bf1cb362cb01eba77de8
CydoEntis <cydoentis@gmail.com>
2023-01-06 16:03:20 -0500

    implemented project list classes

--- src/components/projects/ProjectList.tsx
+++ src/components/projects/ProjectList.tsx
@@ -1,12 +1,19 @@
 import { Project } from "@/types/project-types";
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
 type ProjectListProps = {
   projects: Project[];
 };
 
 const ProjectList = ({ projects }: ProjectListProps) => {
-  return <div>ProjectList</div>;
+  return (
+    <div className="flex flex-wrap gap-5">
+      {projects.map((project) => (
+        <ProjectCard {...project} />
+      ))}
+    </div>
+  );
 };
 
 export default ProjectList;
