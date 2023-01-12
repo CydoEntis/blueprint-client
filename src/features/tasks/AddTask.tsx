9dc7d18cdba7df5847ee22f5c87171c289f85aa2
CydoEntis <cydoentis@gmail.com>
2023-01-12 12:47:36 -0500

    Added AddProject and AddTask components

--- src/features/tasks/AddTask.tsx
+++ src/features/tasks/AddTask.tsx
@@ -1,3 +1,8 @@
+import FormBtn from "@/components/shared/form/FormBtn";
+import FormControl from "@/components/shared/form/FormControl";
+import Input from "@/components/shared/form/Input";
+import Label from "@/components/shared/form/Label";
+import TextArea from "@/components/shared/form/TextArea";
 import Form from "../../components/shared/form/Form";
 import Modal from "../../components/shared/modal/Modal";
 
@@ -6,12 +11,37 @@ type Props = {
 };
 
 const AddTask = ({ onClose }: Props) => {
-  const onSubmit = () => {
+  const handleSubmit = () => {
     console.log("Do something");
   };
   return (
     <Modal onClose={onClose}>
-      <Form onSubmit={onSubmit} title="Create Task" btnText="Create Task" />
+      <Form onSubmit={handleSubmit}>
+        <h1 className="mb-5 text-2xl text-grey-30">New Task</h1>
+        <FormControl>
+          <Label htmlFor="title">Text</Label>
+          <Input id="title" type="text" />
+        </FormControl>
+        <FormControl>
+          <Label htmlFor="description">Description</Label>
+          <TextArea id="description" numCols={20} numRows={10} />
+        </FormControl>
+        <FormControl>
+          <Label htmlFor="users">Assigned Users</Label>
+          <select multiple id="users">
+            <option>Joe</option>
+            <option>Jane</option>
+            <option>Jack</option>
+          </select>
+        </FormControl>
+        <FormControl>
+          <Label htmlFor="date">Due Date</Label>
+          <Input id="date" type="date" />
+        </FormControl>
+        <FormControl>
+          <FormBtn type="submit" text="Add Project" />
+        </FormControl>
+      </Form>
     </Modal>
   );
 };
