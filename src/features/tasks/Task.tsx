62a518b622b446974d09d6288abad5c5d83a4e84
CydoEntis <cydoentis@gmail.com>
2023-01-16 11:13:43 -0500

    Added task header

--- src/features/tasks/Task.tsx
+++ src/features/tasks/Task.tsx
@@ -9,6 +9,7 @@ import SubTasks from "./SubTasks";
 import { AiTwotoneCalendar } from "react-icons/ai";
 import Comments from "../comments/Comments";
 import AddComment from "../comments/AddComment";
+import TaskHeader from "./TaskHeader";
 
 let taskList = [
   {
@@ -84,34 +85,13 @@ const Task = (props: Props) => {
 
   return (
     <div className="relative mx-auto flex w-3/4 flex-wrap gap-3 rounded-2xl bg-white">
-      <div>
-        <Link
-          className="absolute top-5 right-5 flex w-10 items-center justify-center rounded-lg border border-red-30 bg-red-10 py-1 px-2 text-xs text-red-40"
-          to={`/projects/${1}/tasks?sort=all`}
-        >
-          <MdClose className="text-2xl" />
-        </Link>
-
-        <div className="mx-auto w-5/6 rounded-2xl bg-white p-5">
-          <h1 className="text-[2.15rem] font-bold text-grey-40">
-            Mobile App Design
-          </h1>
-          <div className="flex">
-            <Tag text={"ui/ux"} />
-            <Tag text={"mobile"} />
-          </div>
-          <p className=" py-2 text-sm text-grey-20">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            consequuntur libero cumque dicta saepe. Tempore at distinctio non
-            necessitatibus aliquid porro natus adipisci. Ullam consequatur
-            aliquam pariatur. Voluptate, optio assumenda!
-          </p>
-          <div className="flex w-32 items-center rounded-full border border-green-30 bg-green-10 px-3 py-1 text-xs text-green-40">
-            <AiTwotoneCalendar className="mr-1 text-lg" />
-            <p>Feb 16, 2023</p>
-          </div>
-        </div>
-      </div>
+      <Link
+        className="absolute top-5 right-5 flex w-10 items-center justify-center rounded-lg border border-red-30 bg-red-10 py-1 px-2 text-xs text-red-40"
+        to={`/projects/${1}/tasks?sort=all`}
+      >
+        <MdClose className="text-2xl" />
+      </Link>
+      <TaskHeader />
       <SubTasks
         className="mx-auto h-52 w-5/6 overflow-y-auto rounded-2xl bg-white p-3"
         tasks={taskList}
---
+++ src/features/tasks/TaskHeader.tsx
@@ -0,0 +1,30 @@
+import Tag from "@/components/tags/Tag";
+import { AiTwotoneCalendar } from "react-icons/ai";
+
+type Props = {};
+
+const TaskHeader = (props: Props) => {
+  return (
+    <div className="mx-auto w-5/6 rounded-2xl bg-white py-5">
+      <h1 className="text-[2.15rem] font-bold text-grey-40">
+        Mobile App Design
+      </h1>
+      <div className="flex">
+        <Tag text={"ui/ux"} />
+        <Tag text={"mobile"} />
+      </div>
+      <p className=" py-5 text-sm text-grey-20">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
+        consequuntur libero cumque dicta saepe. Tempore at distinctio non
+        necessitatibus aliquid porro natus adipisci. Ullam consequatur aliquam
+        pariatur. Voluptate, optio assumenda!
+      </p>
+      <div className="flex w-32 items-center rounded-full border border-green-30 bg-green-10 px-3 py-1 text-xs text-green-40">
+        <AiTwotoneCalendar className="mr-1 text-lg" />
+        <p>Feb 16, 2023</p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskHeader;
