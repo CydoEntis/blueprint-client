c37f4eb5e4fe342694dcc43a45ea165248f75550
CydoEntis <cydoentis@gmail.com>
2023-01-19 17:33:53 -0500

    Removed unwanted divs in stats container component

--- src/features/dashboard/StatsContainer.tsx
+++ src/features/dashboard/StatsContainer.tsx
@@ -2,16 +2,10 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
-  title: string;
 };
 
-const StatsContainer = ({ title, children }: Props) => {
-  return (
-    <div className=" text-gray-70">
-      <h1 className="mb-3 text-2xl font-bold">{title}</h1>
-      <div className="flex flex-wrap gap-2">{children}</div>
-    </div>
-  );
+const StatsContainer = ({ children }: Props) => {
+  return <div className="flex flex-wrap gap-2">{children}</div>;
 };
 
 export default StatsContainer;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -10,22 +10,22 @@ const index = (props: Props) => {
       <StatsContainer>
         <StatsBlock
           className="flex h-40 w-40 flex-col items-center justify-center rounded-xl border border-accent-30 bg-accent-10 py-2 px-5  text-xl text-accent-40  transition-all  duration-300 ease-in-out  "
-          count={50}
+          count={150}
           text="Total"
         />
         <StatsBlock
           className="flex h-40 w-40 flex-col items-center justify-center rounded-xl border border-yellow-30 bg-yellow-10 py-2 px-5  text-xl text-yellow-40  transition-all  duration-300 ease-in-out  "
-          count={50}
+          count={30}
           text="In Progress"
         />
         <StatsBlock
           className="flex h-40 w-40 flex-col items-center justify-center rounded-xl border border-green-30 bg-green-10 py-2 px-5  text-xl text-green-40  transition-all  duration-300 ease-in-out  "
-          count={50}
+          count={376}
           text="Completed"
         />
         <StatsBlock
           className="flex h-40 w-40 flex-col items-center justify-center rounded-xl border border-red-30 bg-red-10 py-2 px-5  text-xl text-red-40  transition-all  duration-300 ease-in-out  "
-          count={50}
+          count={12}
           text="Past Due"
         />
       </StatsContainer>
