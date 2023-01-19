0616ed673bf159953b6002fa2498af6f78e75d89
CydoEntis <cydoentis@gmail.com>
2023-01-19 17:42:16 -0500

    Added recent tasks to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,14 @@
 import StatsBlock from "@/features/dashboard/StatsBlock";
 import StatsContainer from "@/features/dashboard/StatsContainer";
+import TaskList from "@/features/tasks/TaskList";
+import tasks from "@/data/task-data.json";
 import React from "react";
 
 type Props = {};
 
 const index = (props: Props) => {
   return (
-    <div>
+    <div className=" w-full p-5">
       <StatsContainer>
         <StatsBlock
           className="flex h-40 w-40 flex-col items-center justify-center rounded-xl border border-accent-30 bg-accent-10 py-2 px-5  text-xl text-accent-40  transition-all  duration-300 ease-in-out  "
@@ -29,6 +31,10 @@ const index = (props: Props) => {
           text="Past Due"
         />
       </StatsContainer>
+      <div>
+        <h1>Most Recent Tasks</h1>
+        <TaskList tasks={tasks} />
+      </div>
     </div>
   );
 };
