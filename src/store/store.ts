0aa95150c4eb22c6c6042438f49d0aab86053489
CydoEntis <cydoentis@gmail.com>
2023-01-25 18:39:00 -0500

    Added taskReducer to the redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { TypedUseSelectorHook } from "react-redux/es/types";
 import { configureStore } from "@reduxjs/toolkit";
 import projectReducer from "./features/projectSlice";
+import taskReducer from "./features/taskSlice";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/exports";
 
 export const store = configureStore({
   reducer: {
     project: projectReducer,
+    task: taskReducer,
   },
 });
 
