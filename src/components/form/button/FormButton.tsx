54a6249ef0229b512ca4f7066c99c8288152e5d0
cydoentis <cydoentis@gmail.com>
2023-02-18 10:43:15 -0500

    Recreated Card component

--- src/components/form/button/FormButton.tsx
+++ src/components/form/button/FormButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const FormButton = ({ className, text }: Props) => {
   return (
-    <button className={`w-1/2 rounded-md border sm:w-[200px] capitalize ${className}`}>
+    <button className={` rounded-md border sm:w-[200px] capitalize ${className}`}>
       {text}
     </button>
   );
