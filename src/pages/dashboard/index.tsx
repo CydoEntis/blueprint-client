52cd5646539b8d37dda704fc81cd2e22932cadd2
cydoentis <cydoentis@gmail.com>
2023-02-26 18:40:50 -0500

    Added graph title

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,17 @@ import StatsBlock from "@/features/dashboard/StatsBlock";
 import StatsContainer from "@/features/dashboard/StatsContainer";
 import Title from "@/components/typography/Typography";
 import { useAppSelector } from "@/store/store";
+import {
+  ResponsiveContainer,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+} from "recharts";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type Props = {};
 
@@ -9,13 +20,27 @@ const index = (props: Props) => {
   const pending = useAppSelector((state) => state.job.pending);
   const declined = useAppSelector((state) => state.job.declined);
   const interview = useAppSelector((state) => state.job.interview);
-  const user = useAppSelector(state => state.user.user);
+  const user = useAppSelector((state) => state.user.user);
+
+  const [monthlyApps, setMonthlyApps] = useState();
+
+  useEffect(() => {
+    async function getMonthlyApps() {
+      const res = await axios("http://localhost:1337/jobs/apps");
+      setMonthlyApps(res.data.monthlyApplications);
+    }
+
+    getMonthlyApps();
+  }, []);
+
   return (
     <div className=" w-full p-5">
-      <Title className="p-5 text-[2rem]" text={`Good afternoon, ${user.username}`} />
-      <StatsContainer className="flex w-full flex-wrap md:flex-nowrap items-center justify-center gap-5 p-5 ">
+      <Title
+        className="p-5 text-[2rem]"
+        text={`Good afternoon, ${user.username}`}
+      />
+      <StatsContainer className="flex w-full flex-wrap items-center justify-center gap-5 p-5 md:flex-nowrap ">
         <StatsBlock
-
           className=" border-yellow-30 bg-yellow-10  text-yellow-40"
           count={pending}
           text="pending applications"
@@ -31,14 +56,18 @@ const index = (props: Props) => {
           text="jobs declined"
         />
       </StatsContainer>
-      {/* <RecentItems>
-        <Title className="ml-5 text-2xl" text="Recent Projects" />
-        <ProjectList projects={projects} />
-      </RecentItems>
-      <RecentItems>
-        <Title className="ml-5 text-2xl" text="Recent Tasks" />
-        <TaskList tasks={tasks} />
-      </RecentItems> */}
+      <div className="hidden w-full md:block mt-24">
+        <h3 className="p-5 text-2xl text-grey-40 dark:text-off-white">Job Applications Breakdown</h3>
+        <ResponsiveContainer width="100%" height={500}>
+          <BarChart data={monthlyApps} margin={{ top: 25 }}>
+            <CartesianGrid strokeDasharray="3 3 " />
+            <XAxis dataKey="date" />
+            <YAxis allowDecimals={false} />
+            <Tooltip />
+            <Bar dataKey="count" fill="#00B0D6" barSize={75} />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
